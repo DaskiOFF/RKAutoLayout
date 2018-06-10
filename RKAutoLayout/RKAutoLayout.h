6ff5963670909589bc19ee2de1451681e4c7bab6
Roman Kotov <waydeveloper@gmail.com>
2018-06-10 20:59:52 +0300

    Initial Commit

---
+++ RKAutoLayout/RKAutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  RKAutoLayout.h
+//  RKAutoLayout
+//
+//  Created by Roman Kotov on 10/06/2018.
+//  Copyright © 2018 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKAutoLayout.
+FOUNDATION_EXPORT double RKAutoLayoutVersionNumber;
+
+//! Project version string for RKAutoLayout.
+FOUNDATION_EXPORT const unsigned char RKAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKAutoLayout/PublicHeader.h>
+
+
