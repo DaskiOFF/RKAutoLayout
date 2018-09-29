212e2adc3a2ef0bf3a39430fb00a204ab28fe647
Roman Kotov <waydeveloper@gmail.com>
2018-09-29 23:05:51 +0300

    Add RKConstaintSize tests

--- RKAutoLayoutTests/Size/RKConstraintSizeTests.swift
+++ RKAutoLayoutTests/Size/RKConstraintSizeTests.swift
@@ -1,12 +1,7 @@
-//
-//  RKConstraintSizeTests.swift
-//  RKAutoLayoutTests
-//
-//  Created by Roman Kotov on 27/09/2018.
-//  Copyright © 2018 Roman Kotov. All rights reserved.
-//
-
 import XCTest
+import UIKit
+
+@testable import RKAutoLayout
 
 class RKConstraintSizeTests: XCTestCase {
 
@@ -18,5 +13,261 @@ class RKConstraintSizeTests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
+    }
+
+    func testResultIsCorrectWidthBuilder() {
+        do {
+            let builderHolder = RKConstraintSize.width(10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == nil)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .equal)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(view, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.widthAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .equal)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(view.heightAnchor, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.heightAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .equal)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(min: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == nil)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .greaterThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(min: view, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.widthAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .greaterThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(min: view.heightAnchor, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.heightAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .greaterThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(max: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == nil)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .lessThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(max: view, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.widthAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .lessThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.width(max: view.heightAnchor, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintWidthBuilder else {
+                XCTFail("Builder must be RKConstraintWidthBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.heightAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .lessThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+    }
+
+    func testResultIsCorrectHeightBuilder() {
+        do {
+            let builderHolder = RKConstraintSize.height(10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == nil)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .equal)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(view, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.heightAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .equal)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(view.widthAnchor, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.widthAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .equal)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(min: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == nil)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .greaterThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(min: view, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.heightAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .greaterThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(min: view.widthAnchor, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.widthAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .greaterThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(max: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == nil)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .lessThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(max: view, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.heightAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .lessThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
+
+        do {
+            let builderHolder = RKConstraintSize.height(max: view.widthAnchor, multiplier: 10, priority: .low, isActive: false)
+            guard let builder = builderHolder.builder as? RKConstraintHeightBuilder else {
+                XCTFail("Builder must be RKConstraintHeightBuilder type")
+                return
+            }
+
+            XCTAssert(builder.anchor == view.widthAnchor)
+            XCTAssert(builder.value == 10)
+            XCTAssert(builder.relation == .lessThanOrEqual)
+            XCTAssert(builder.priority.value == RKConstraintPriority.low.value)
+            XCTAssert(builder.isActive == false)
+        }
     }
 }
